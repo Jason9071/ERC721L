d3b6e6746e61a5ef24d90ee0404361651f470e39
Jason <82122083+Jason9071@users.noreply.github.com>
2022-04-25 19:17:53 +0800

    feat : withdrawal step
    1 : owner requestToWithdraw
    2 : holder vote
    3 : owner withdraw

--- ERC721L/ERC721L.sol
+++ ERC721L/ERC721L.sol
@@ -7,33 +7,49 @@ pragma solidity ^0.8.4;
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract ERC721L is ERC721A, Ownable{
-    constructor( 
-        bytes32 merkleRoot_
-     ) ERC721A("ERC721L", "Lock", "ipfs:///") {
-        merkleRoot = merkleRoot_;
-        vtw.allowToRequestToWithdraw = true ;
-        vtw.requestingWithdraw = false ;
-        vtw.percentage = 0 ;
-        vtw.timeLock = 0 ;
-    }
+    using SafeMath for uint256 ;
 
     struct voteToWithdraw{
         bool allowToRequestToWithdraw ;
         bool requestingWithdraw ;
+        uint256 passRate ;
         uint percentage ;
         uint256 timeLock ;
-        mapping( uint256 => bool ) vote ;
+        mapping( uint256 => bool ) isVoted ;
+        uint256 vote ;
     }
 
     voteToWithdraw public vtw;
-    bytes32 public merkleRoot;
-    uint256 public price = 0.1 ether ; 
-    function mint(address to, uint256 quantity) internal {
-        _safeMint(to, quantity);
+    //bytes32 public merkleRoot;
+    uint256 public maxSupply; 
+    uint256 public price; 
+
+    constructor( 
+        //bytes32 merkleRoot_,
+        //uint256 price_,
+        //uint256 maxSupply_
+     ) ERC721A("ERC721L", "Lock", "ipfs://test/") {
+        //merkleRoot = merkleRoot_;
+        //price = price_;
+        price = 0.3 ether;
+        maxSupply = 3333;
+        vtw.allowToRequestToWithdraw = true ;
+        vtw.requestingWithdraw = false ;
+        vtw.passRate = 65 ;
+        vtw.percentage = 0 ;
+        vtw.timeLock = 0 ;
+        vtw.vote = 0 ;
     }
 
+    function mint(uint256 quantity) external payable {
+        require( msg.value * quantity >= price ,"ERC721L: no enough ether");
+        _safeMint(msg.sender, quantity);
+    }
+
+    /*
     function leaf(address _account) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(_account));
     }
@@ -45,8 +61,9 @@ contract ERC721L is ERC721A, Ownable{
     function setTokenURI(string memory newTokenURI) external onlyOwner {
         _setTokenURI(newTokenURI);
     }
+    */
 
-    function requestToWithdraw( uint percentage ) public onlyOwner {
+    function requestToWithdraw( uint percentage ) external onlyOwner {
         require(address(this).balance > 0, "ERC721L: insufficient balance");
         require(vtw.allowToRequestToWithdraw, "ERC721L: not allowed to request withdraw");
         require(percentage > 1, "ERC721L: percentage must be bigger then 0");
@@ -66,15 +83,49 @@ contract ERC721L is ERC721A, Ownable{
         }
     }
 
+    function vote( uint256 tokenId ) external {
+        require(vtw.requestingWithdraw, "ERC721L: no withdraw request applied");
+        require(vtw.timeLock > block.timestamp, "ERC721L: voting is over now");
+        require( ownerOf(tokenId) == msg.sender , "ERC721L: you are not the token owner");
+        require( !vtw.isVoted[tokenId] , "ERC721L: this token already voted");
+
+        vtw.isVoted[tokenId] = true ;
+        vtw.vote++ ;
+    }
+
+    function pass() internal view returns( bool ) {
+        uint256 passVotes = _totalMinted().div(100).mul(65) ;
+        if ( vtw.vote >= passVotes )
+            return false ;
+        return true;
+    }
 
     function withdraw() public onlyOwner {
         require(address(this).balance > 0, "ERC721L: insufficient balance");
         require(vtw.requestingWithdraw, "ERC721L: no withdraw request applied");
         require(vtw.timeLock < block.timestamp, "ERC721L: waitting holder to vote");
-
         
-        payable(msg.sender).transfer(address(this).balance);
+        if ( pass() )
+            payable(owner()).transfer(address(this).balance.div(100).mul(vtw.percentage));
 
+        // reset all the info //
+        vtw.allowToRequestToWithdraw = true ;
+        vtw.requestingWithdraw = false ;
+        vtw.passRate = 65 ;
+        vtw.percentage = 0 ;
+        vtw.timeLock = 0 ;
+        vtw.vote = 0 ;
+        // reset all the info //
     }
-    
+
+    function voteInfo() external view returns( bool, bool, uint, uint, uint256, uint256 ) {
+        return ( 
+            vtw.allowToRequestToWithdraw,
+            vtw.requestingWithdraw,
+            vtw.passRate,
+            vtw.percentage,
+            vtw.timeLock,
+            vtw.vote
+        );
+    }
 }
